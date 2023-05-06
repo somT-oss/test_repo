d6c6d50fa452820c6659793429af90e56abb6c91
somT-oss <somtochukwuuchegbu@gmail.com>
2023-05-06 20:54:59 +0100

    push

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-def write_file(content: str, file: str) -> str: 
-    with open(file, "w") as f:
-        f.write(content)
-        return "Successfully wrote to the file " + file
-
-
-def read_file(file_name: str) -> str:
-    with open(file_name) as f:
-        for line in f:
-            return line
-
-write = write_file("New Content", "new.txt")
-print(write)
+print("Hello World")
